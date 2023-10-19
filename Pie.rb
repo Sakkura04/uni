fb5c75f13e0093596cd7665d14f7ce6d738eaed3
Soroka bird <vladavlada09756@gmail.com>
2023-10-19 11:26:16 +0300

    Update Pie.rb

--- Pie.rb
+++ Pie.rb
@@ -1,41 +1,32 @@
-def pause_until_1
-  puts "Press '1' to continue..."
-  input = gets.chomp
-  until input == '1'
-    puts "Press '1' to continue..."
-    input = gets.chomp
-  end
-end
-
-
-def search_izum(pie, width, height)
+def search_izum(pie, start, width, height)
   i = 1
   izum = 0
 
   pie.map do |subarray|
-    izum += subarray.map { |str| str[0..width].count('o') }.sum
+    izum += subarray.map { |str| str[start..width].count('o') }.sum
     break if i == height
     i += 1
   end
-
   return true if izum == 1
-  return false
+  false
 end
 
 
 def cut_pie(pie, start, ends, height)
   i = 0
+  cut_pie = pie.map { |subarray| [subarray[0].dup] }  # Створюємо глибокі копії підмасивів
   piece = []
 
-  pie.each do |subarray|
+  cut_pie.each do |subarray|
     piece[i] = []
     (start..ends).each do |index|
       piece[i] << subarray[0][index]
+      subarray[0][index] = "0"
     end
     break if i == height-1
     i += 1
   end
-  [pie, piece]
+  [cut_pie, piece]
 end
 
 
@@ -44,71 +35,95 @@ def check_line(pie)
   pie
 end
 
+def count_izum(pie)
+  count = 0
+  pie.each do |subarray|
+    subarray[0].each_char do |char|
+      count += 1 if char == "o"
+    end
+  end
+  count
+end
+
+def total_area(arrays)
+  count = 0
+  arrays.each do |subarray|
+    subarray.each do |string|
+      count += string.count('^0')
+    end
+  end
+  return count
+end
+
+
+
+
+
+def is_2d_array?(array)
+  # Перевіряємо, чи array є екземпляром класу Array і чи його елементи також є масивами
+  array.is_a?(Array) && array.all? { |subarray| subarray.is_a?(Array) }
+end
 
 def divide_rectangle(pie)
   width = pie[0][0].chars.count { |char| char != "0" }
   start = pie[0][0].chars.index { |char| char != "0" }
   height = pie.size
-  num_parts = pie.flatten.count {|izum| izum == "o"}
-  total_area = width * height
-  part_area = total_area / num_parts
-
+  num_parts = count_izum(pie)
+  total_area = total_area(pie)
+  part_area = total_area / num_parts.to_f
 
   (1..width).each do |w_part|
-
     h_part = part_area / w_part
     next if h_part != h_part.to_i || h_part > height
 
-    if search_izum(pie, w_part, h_part)
-      res = cut_pie(pie, start, w_part, h_part)
-      cuted_pie = res[0]
+    if search_izum(pie, start, w_part + start - 1, h_part)
+      res = cut_pie(pie.dup, start, w_part - 1 + start, h_part)
       pieces = res[1]
-      cuted_pie = check_line(pie)
+      cuted_pie = check_line(res[0])
+
+      # puts pieces.inspect
+      # puts "\n\n"
+      # puts cuted_pie.inspect
+
       unless cuted_pie.empty?
-        pieces << divide_rectangle(cuted_pie)
+        recurs = divide_rectangle(cuted_pie)
+        if !is_2d_array?(recurs)
+          # puts pie.inspect
+          # puts cuted_pie.inspect
+          next
+        else
+          pieces << recurs
+        end
       end
       return pieces
     end
   end
-
-
 end
 
 
-def print_divisions(divisions)
-  divisions.each do |division|
-    puts "Розділити на #{division.size} прямокутників:"
-    division.each do |w, h|
-      puts "  #{w}x#{h}"
+def pretty_print_array(arr, indent = 0)
+  arr.each do |item|
+    if item.is_a?(Array)
+      pretty_print_array(item, indent + 1)
+    else
+      puts "  " * indent + item
     end
-    puts
   end
 end
 
 
 
-
-# ....
-# .o..
-# ....
-puts("\nEnter your pie, to finish enter 1")
-pies = ""
-pie = []
-while true
-  pies = gets().split("\n")
-  break if pies == ["1"]
-  pie << pies
-end
-
-
-# divisions = nil
-#
-# if (height * width) % num_parts == 0
-#   divisions = divide_rectangle(pie, possible_divisions)
+# puts("\nEnter your pie, to finish enter 1")
+# pie = []
+# while true
+#   pies = gets().split("\n")
+#   break if pies == ["1"]
+#   pie << pies
 # end
-#
-# if divisions.empty?
-#   puts "Неможливо розділити прямокутник на #{num_parts} частин з однаковою площею."
-# else
-#   print_divisions(divisions)
-# end
+
+array = [["o.."], [".o."], ["..o"]]
+# array = [["o..."], [".o.."], ["...."], ["o..o"], ["..o."]]
+# array = [[".o.o...."], ["........"], ["....o..."], ["........"], [".....o.."], ["........"]]
+res = divide_rectangle(array)
+puts res.inspect
+# pretty_print_array(res, depth = 1)
